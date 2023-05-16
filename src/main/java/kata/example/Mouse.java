1b3b8ff84a82dd60df7bfb9a1f9c565e8e4c2348
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-05-17 00:41:07 +0100

    🟢 MouseShould not_notify_any_from_left_button_if_this_was_not_pressed

--- src/main/java/kata/example/Mouse.java
+++ src/main/java/kata/example/Mouse.java
@@ -5,12 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mouse {
+    boolean leftButtonIsPressed = false;
     private List<MouseEventListener> listeners = new ArrayList<>();
     private final long timeWindowInMillisecondsForDoubleClick = 500;
     public void pressLeftButton(long currentTimeInMilliseconds) {
+        leftButtonIsPressed = true;
     }
 
     public void releaseLeftButton(long currentTimeInMilliseconds) {
+        if (!leftButtonIsPressed) {
+            return;
+        }
         notifySubscribers(MouseEventType.CLICK);
     }
 
