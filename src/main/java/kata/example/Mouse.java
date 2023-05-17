f341cb61a4e6d3707941b86450bf4bffafbada93
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-05-17 01:12:15 +0100

    🟢 MouseShould notify_double_click_when_time_window_between_last_realease_and_following_press_on_left_button_is_short  MouseShould notify_double_click_when_time_window_between_last_realease_and_following_press_on_left_button_is_short

--- src/main/java/kata/example/Mouse.java
+++ src/main/java/kata/example/Mouse.java
@@ -6,17 +6,30 @@ import java.util.List;
 
 public class Mouse {
     boolean leftButtonIsPressed = false;
+    long lastTimeLeftButtonWasPresssed = -1;
+    long lastTimeLeftButtonWasReleased = 0;
+    boolean isDoubleClick = false;
     private List<MouseEventListener> listeners = new ArrayList<>();
     private final long timeWindowInMillisecondsForDoubleClick = 500;
     public void pressLeftButton(long currentTimeInMilliseconds) {
         leftButtonIsPressed = true;
+        if( (lastTimeLeftButtonWasPresssed >= 0) && (currentTimeInMilliseconds - lastTimeLeftButtonWasReleased) < timeWindowInMillisecondsForDoubleClick) {
+            isDoubleClick = true;
+        }
+        lastTimeLeftButtonWasPresssed = currentTimeInMilliseconds;
     }
 
     public void releaseLeftButton(long currentTimeInMilliseconds) {
         if (!leftButtonIsPressed) {
             return;
         }
-        notifySubscribers(MouseEventType.CLICK);
+        if(isDoubleClick) {
+            notifySubscribers(MouseEventType.DOUBLE_CLICK);
+        }else{
+            notifySubscribers(MouseEventType.CLICK);
+        }
+        lastTimeLeftButtonWasReleased = currentTimeInMilliseconds;
+        leftButtonIsPressed = false;
     }
 
     public void move(
